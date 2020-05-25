0c45a319bfe786f40535a502aa50667f36a2b878
Charlie Hayes <cjhayestutor@gmail.com>
2020-05-25 19:32:53 -0400

    Updated to use 'head' to get the first item in an array.

---
+++ head.js
@@ -0,0 +1,13 @@
+function head(array){
+    return array[0];
+}
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    }
+  };
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
