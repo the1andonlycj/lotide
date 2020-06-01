2abebbbfc844708aa7bef1f94f84cce3571e82ee
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-01 16:20:52 +0000

    Added module functionality to assertEqual and head, but also made their testing functionality separate

--- head.js
+++ head.js
@@ -1,13 +1,11 @@
+const assertEqual = require('./assertEqual');
+
+
+
 function head(array){
     return array[0];
 }
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-    }
-  };
+
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
